748e67fbc61ebefdbda7a540a5741d6e3a2b0155
ishika matta <imatta09@gmail.com>
2022-03-15 19:05:11 -0400

    react-router-DOM 6

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+    return(
+        <div className='home'>
+            <h1 className="title">
+                Home
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
