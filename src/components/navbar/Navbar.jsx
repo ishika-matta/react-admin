1d37efb1ea55aa2b5130ebf37525fa0df628ca46
ishika matta <imatta09@gmail.com>
2022-03-15 20:25:52 -0400

    sidebar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.scss';
+
+const Navbar = () => {
+    return(
+        <div className='navbar'>
+            <h1 className="title">
+                Home
+            </h1>
+        </div>
+    )
+}
+
+export default Navbar;
